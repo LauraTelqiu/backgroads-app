a71b338852afe2eec614a07158ae57710aaec875
Laura <lauratelqiu@gmail.com>
2023-01-22 13:58:34 -0500

    commit

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import Title from "./Title"
-import { tours } from '../data'
-
-
+import React from "react";
+import Title from "./Title";
+import { tours } from "../data";
 
 function Tours() {
   return (
     <section className="section" id="tours">
-      <Title title='featured' subtitle='tours' />
+      <Title title="featured" subtitle="tours" />
 
-      <div className='section-center featured-center'>
+      <div className="section-center featured-center">
         {tours.map((tour) => {
-          const { id, image, date, title, info, location, duration, cost } = tour
+          const { id, image, date, title, info, location, duration, cost } =
+            tour;
           return (
-
             <article key={id} className="tour-card">
               <div className="tour-img-container">
                 <img src={image} className="tour-img" alt={title} />
@@ -24,33 +22,24 @@ function Tours() {
                 <div className="tour-title">
                   <h4>{title}</h4>
                 </div>
-                <p>
-                  {info}
-                </p>
+                <p>{info}</p>
                 <div className="tour-footer">
                   <p>
-                    <span><i className="fas fa-map"></i></span> {location}
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {location}
                   </p>
                   <p>{duration} days</p>
                   <p> from ${cost}</p>
                 </div>
               </div>
-
             </article>
-
-
-
-          )
+          );
         })}
-
       </div>
-
-
-
-    </section >
-
-  )
-
+    </section>
+  );
 }
 
 export default Tours;
